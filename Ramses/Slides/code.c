e25a819910818cceeb0fb18e4ec59e6f0252cfdf
MaterialAlien <rrafaeljuniorr@gmail.com>
2017-08-26 22:34:06 -0300

    teefe
    gt45

---
+++ Ramses/Slides/code.c
@@ -0,0 +1,38 @@
+
+
+
+{ Caso 1
+	unsigned char destino;
+	unsigned char *p;
+
+	destino = *p;
+}
+
+{ Caso 2
+	unsigned char destino;
+
+	destino = 5;
+}
+
+{ Caso 3
+	unsigned char vet[10];
+	unsigned char destino;
+	unsigned char index;
+
+	destino = vet[index];
+}
+
+{ Caso4
+	unsigned char destino;
+
+	destino = destino>>1;
+}
+
+{ Caso 5
+	funcao();
+	funcao();
+}
+
+
+
+
